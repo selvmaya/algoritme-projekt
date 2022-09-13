308805ac5d4de4eb2a242920ff5a05ef5265682e
Maya <selvmaya@proton.me>
2022-09-13 13:12:32 +0200

    update to include random tools etc

--- Algoritmer/GameForm.cs
+++ Algoritmer/GameForm.cs
@@ -2,21 +2,28 @@ namespace Algoritmer
 {
     public partial class GameForm : Form
     {
+		public enum CardType
+		{
+			Hearts,
+			Diamonds,
+			Spades,
+			Clubs,
+		}
 	    private const int CardCount = 13;
 	    private const int DeckSize = 52;
 
-	    private static readonly Random Random = new Random();
-
 	    private readonly Deck _mainDeck;
-	    private Graphics _graphics;
-	    private Pen cardPen;
+	    private readonly Graphics _graphics;
+	    private readonly Pen _cardPen;
+
+	    private static readonly Random Random = new Random();
 
 	    public GameForm() // effectively start
         {
 	        InitializeComponent();
 
 	        _graphics = CreateGraphics();
-	        cardPen = new Pen(Color.Red, 5);
+	        _cardPen = new Pen(Color.Red, 5);
 
 	        _mainDeck = new Deck();
 	        _mainDeck.Shuffle();
@@ -26,80 +33,89 @@ namespace Algoritmer
         {
 			BackColor = Color.Black;
 
-			DrawDeck();
+			_mainDeck.Draw();
         }
 
-        private void DrawDeck()
-        {
-	        // todo: define deck drawing area (placement/size)
-
-	        foreach (Card card in _mainDeck.Cards)
-	        {
-		        DrawCard(card);
-	        }
-        }
-
-        private void DrawCard(Card card)
-        {
-	        Rectangle rect = new Rectangle(); // todo: define card placement/size
-			_graphics.DrawRectangle(cardPen, rect);
-
-			// todo: draw card number/name
-        }
 
         private class Deck
         {
 	        public readonly List<Card> Cards;
 
-	        public Deck() // fresh deck
+	        public Deck() // fresh deck of cards
 	        {
 		        Cards = new List<Card>();
 		        const int amountOfCardsPerType = DeckSize / CardCount;
-		        for (int cardTypeIndex = 0; cardTypeIndex < CardCount; cardTypeIndex++)
+		        for (int cardNumberIndex = 0; cardNumberIndex < CardCount; cardNumberIndex++)
 		        {
 			        for (int type = 0; type < amountOfCardsPerType; type++)
 			        {
-				        Cards.Add(new Card(cardTypeIndex));
+				        Cards.Add(new Card(cardNumberIndex));
 			        }
 		        }
 	        }
 
-	        public Deck(List<Card> cards)
-	        {
-		        Cards = cards;
-	        }
+	        // public Deck(List<Card> cards) // Construction from cards
+	        // {
+		       //  Cards = cards;
+	        // } // note: probably shouldnt want this, logic-wise?
 
 	        public void Shuffle()
 	        {
 		        for (int i = 0; i < DeckSize; i++)
 		        {
 			        int randomIndex = Random.Next(DeckSize);
-			        // swap cards
+			        // swap card index with random card index
 			        (Cards[i], Cards[randomIndex]) = (Cards[randomIndex], Cards[i]);
 		        }
 	        }
+
+	        public void Draw()
+			{
+				// todo: define deck drawing area (placement/size)
+
+				foreach (Card card in Cards)
+				{
+					card.Draw();
+				}
+			}
         }
 
         private class Card
         {
-	        public readonly int Value;
 
-	        public Card(int value)
+	        public readonly CardType Type;
+	        public readonly int Number;
+
+	        public Card(int number, CardType type)
 	        {
-		        Value = value;
+		        Number = number;
+		        Type = type;
 	        }
 
-	        public string Name()
+	        public string NumberName()
 	        {
-		        return Value switch
+		        return Number switch
 		        {
+			        1 => "Ace",
 			        11 => "Jack",
 			        12 => "Queen",
 			        13 => "King",
-			        _ => Value.ToString(),
+			        _ => Number.ToString(),
 		        };
 	        }
 
+	        public string FullName()
+	        {
+		        return $"{NumberName()} of {Type}";
+	        }
+
+	        public void Draw()
+			{
+				Rectangle rect = new Rectangle(); // todo: define card placement/size
+				 _graphics.DrawRectangle(_cardPen, rect);
+
+				 // todo: draw card number/name
+			}
         }
     }
 }
---
+++ Algoritmer/RandTools.cs
@@ -0,0 +1,23 @@
+﻿namespace Algoritmer;
+
+public static class RandTools
+{
+	public static int RandomRange(this Random random, int min, int max)
+	{
+		int range = max - min;
+		double variance = range * random.NextDouble();
+		return min + (int)variance;
+	}
+	public static float RandomRange(this Random random, float min, float max)
+	{
+		float range = max - min;
+		double variance = range * random.NextDouble();
+		return min + (float)variance;
+	}
+	public static double RandomRange(this Random random, double min, double max)
+	{
+		double range = max - min;
+		double variance = range * random.NextDouble();
+		return min + variance;
+	}
+}
